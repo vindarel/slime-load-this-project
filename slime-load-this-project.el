68e4ba4023108d449aec382f96d00d4b66f44b10
vindarel <vindarel@mailz.org>
2021-01-19 15:54:49 +0100

    decompose. System still inferred from asd filename.

--- slime-load-this-project.el
+++ slime-load-this-project.el
@@ -3,14 +3,19 @@
 (require 'f)
 (require 's)
 
+(defun get-system-name (asds)
+  "Return the system name to load."
+  (first (s-split "\\." (f-filename (first asds)))))
+
 (defun slime-load-this-project ()
   "Find the asd declaration at the project root,
-  load the file and load the system.
+  load the .asd and load the system.
 
-  The system named is inferred from the file name. This can be improved."
+  The system name is inferred from the file name. This can be improved."
   (interactive)
-  (let ((asds (f-glob "*.asd" (projectile-project-root))))
-    (when asds
-      (when (yes-or-no-p (concat "Load file " (first asds) " and system XXX" "?"))
-        (when (slime-load-file (first asds))
-          (slime-load-system (first (s-split "\\." (f-filename (first asds))))))))))
+  (let* ((asds (f-glob "*.asd" (projectile-project-root)))
+         (system (get-system-name asds)))
+    (when (and asds
+               (yes-or-no-p (concat "Load file " (first asds) " and system " system "?"))
+               (slime-load-file (first asds)))
+      (slime-load-system system))))
